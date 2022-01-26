cd1c17a5adb4129f0536de37e687b5d6a3241259
Versantos <veronicasilvamagalhaes@gmail.com>
2022-01-26 20:09:01 -0300

    Finalizado

---
+++ script.js
@@ -0,0 +1,25 @@
+function mensagem (hello){
+    console.log(`Hi, good night! ${hello}`);
+}
+mensagem ("Bye bye")
+
+function mostrarNome(nome){
+    console.log(`Meu nome é  ${nome}`);
+}
+mostrarNome("Veronica")
+
+function selfie(nome, idade, estilo){
+    console.log(`Meu nome é ${nome} tenho ${idade} e gosto ${estilo}`);
+}
+selfie ("Veronica","18", "Rock");
+
+function entreterimento(filme, série, música){
+    console.log(`O filme cotado ${filme} lançamento da quarta temporada ${série} e hit do momento ${música}`);
+}
+entreterimento ("Encantado","The good doctor", "Socadona");
+
+
+function triplo(num){
+return num*3;
+} console.log(triplo(3))
+triplo(3);
